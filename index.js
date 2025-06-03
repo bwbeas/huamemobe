39274c461aec3362ffd3faca45e63dfdcc25f7b5
bellbuuu <beutokyo@gmail.com>
2025-06-03 12:42:03 +0530

    Initial backend setup

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const authMiddleware = require("./authMiddleware");
 
 const express = require("express");
@@ -14,10 +16,12 @@ const SECRET_KEY = "blair";
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect("mongodb://localhost:27017/personaldiary")
-  .then(() => console.log("Connected to MongoDB"))
-  .catch(err => console.error("MongoDB error:", err));
-
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => console.log("MongoDB connected"))
+.catch(err => console.error("MongoDB connection error:", err));
 
 
 app.post("/register", async (req, res) => {
